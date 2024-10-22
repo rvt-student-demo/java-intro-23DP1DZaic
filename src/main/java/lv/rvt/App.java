050edc832247080e7550f4fe8c46b315e72a0aa9
23DP1DZaic <166118246+23DP1DZaic@users.noreply.github.com>
2024-10-22 07:19:19 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,6 +3,7 @@ package lv.rvt;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 
@@ -11,99 +12,31 @@ public class App
 {
     public static void main( String[] args )
     {
-    //     // // My code starts here
+    //  My code starts here
+Scanner scanner = new Scanner(System.in);
+ArrayList<Integer> num = new ArrayList<>();
 
-        Scanner scanner = new Scanner(System.in);
-        printStars(5);
-        printStars(3);
-        printStars(9);
-
-        printSquare(4);
-
-        printRectangle(17, 3);
-
-        printTriangle(4);
-
-        printTriangleTwo(4);
-
-        christmasTree(10);
-
-
+System.out.println("Enter numbers (enter -1 to stop):");
+while (true) {
+    int input = scanner.nextInt();
+    if (input == -1) {
+        break;
     }
 
-
-
-public static void printStars(int number) {
-for (int i = 0; i < number; i = i + 1) {
-System.out.print("*");
-}
-System.out.println("");
-}
-
-
-
-public static void printSquare(int size) {
-  System.out.println("");
-for (int i = 0; i < size; i++) {
-  printStars(4);
-}
-System.out.println("");
-}
-
-
-
-public static void printRectangle(int width, int height) {
-for ( int i = 0; i < height; i++) {
-  printStars(width);
-}
-System.out.println("");
-}
-
-
-
-public static void printTriangle(int size) {
-for (int i = 0; i < size + 1; i++) {
-  printStars(i);
-}
-System.out.println("");
-}
-
-
-public static void printSpaces(int number) {
-  for (int i = 0; i < number; i = i + 1) {
-    System.out.print(" ");
+num.add(input);
     }
-}
+        
+        
+System.out.println("From where?");
+int startIndex = scanner.nextInt();
+System.out.println("To where?");
+int endIndex = scanner.nextInt();
+        
+        
+ System.out.println("Numbers in the range:");
+ for (int i = startIndex; i <= endIndex; i++) {
+     System.out.println(num.get(i));
+        }
+        }
 
-public static void printTriangleTwo(int size) {
-for (int i = 1; i < size + 1; i++) {
-printSpaces(size - i);
-printStars(size - (size - i));
-}
-
-System.out.println("");
-}
-
-
-public static void christmasTree(int height) {
-  int count = 1;
-  int countStars = 1;
-  
-
-
-   for( int i = 1; i <= height; i++) {
-      printSpaces(height-count);
-      printStars(countStars);   
-      count++;
-      countStars += 2;
-  }
-  
-  printSpaces(height-2);
-  printStars(3);
-  printSpaces(height-2);
-  printStars(3);
-
-
-System.out.println("");
-}
 }
